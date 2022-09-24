51f85255e73ce1fb59158791dca7eeb56326e836
Simon Blessenohl <simon.blessenohl@deepl.com>
2022-09-24 11:57:39 +0200

    Do not crash on URLs that miss `http(s)://`

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -11,7 +11,11 @@ async fn get(url: Url) -> Result<String, reqwest::Error> {
 }
 
 pub async fn extract(url: &str) -> Result<Vec<Event>, Box<dyn Error>> {
-  let url_parsed = Url::parse(url)?;
+  let mut fully_specified_url: String = url.to_string();
+  if !url.starts_with("http") {
+    fully_specified_url = format!("http://{}", url);
+  } 
+  let url_parsed = Url::parse(&fully_specified_url)?;
   let website_code = get(url_parsed).await?;
   Ok(LargestElementWithSingleDateExtractor::code_to_events(&website_code, &Utc::now().naive_utc().date()))
 }
